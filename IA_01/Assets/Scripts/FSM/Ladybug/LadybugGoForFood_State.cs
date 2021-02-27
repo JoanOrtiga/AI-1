387714ff66ae9503651c9c1bf6b85812f2410cad
JoanStark <47462893+JoanStark@users.noreply.github.com>
2021-02-27 23:17:39 +0100

    .

--- IA_01/Assets/Scripts/FSM/Ladybug/LadybugGoForFood_State.cs
+++ IA_01/Assets/Scripts/FSM/Ladybug/LadybugGoForFood_State.cs
@@ -8,12 +8,13 @@ using FSM;
 using Steerings;
 
 [RequireComponent(typeof(LadybugBlackboard))]
+[RequireComponent(typeof(LadybugManageFood_State))]
 [RequireComponent(typeof(ArrivePlusAvoid))]
 public class LadybugGoForFood_State : FiniteStateMachine
 {
     public enum State
     {
-        INITIAL, PERSUE, BRINGBASE, EAT
+        INITIAL, PERSUE, MANAGEFOOD
     }
 
     public State currentState = State.INITIAL;
@@ -21,26 +22,27 @@ public class LadybugGoForFood_State : FiniteStateMachine
     private LadybugBlackboard lbBlackboard;
 
     private ArrivePlusAvoid arriveAvoid;
+    private LadybugManageFood_State manageFood;
 
     private void Awake()
     {
         lbBlackboard = GetComponent<LadybugBlackboard>();
 
         arriveAvoid = GetComponent<ArrivePlusAvoid>();
+        manageFood = GetComponent<LadybugManageFood_State>();
     }
 
     public override void Exit()
     {
         arriveAvoid.enabled = false;
         arriveAvoid.target = null;
-
+        manageFood.Exit();
         base.Exit();
     }
 
     public override void ReEnter()
     {
         currentState = State.INITIAL;
-
         base.ReEnter();
     }
 
@@ -55,6 +57,8 @@ public class LadybugGoForFood_State : FiniteStateMachine
                 if (SensingUtils.DistanceToTarget(gameObject, lbBlackboard.antTarget) < lbBlackboard.distanceToKill)
                 {
                     lbBlackboard.antTarget.transform.parent = transform;
+                    ChangeState(State.MANAGEFOOD);
+                    /*
                     if (lbBlackboard.hunger > lbBlackboard.needToEatThreshold)
                     {
                         ChangeState(State.EAT);
@@ -63,11 +67,11 @@ public class LadybugGoForFood_State : FiniteStateMachine
                     {
                         ChangeState(State.BRINGBASE);
                     }
-
+                    */
                 }
                 break;
-            case State.BRINGBASE:
-                if (lbBlackboard.hunger > lbBlackboard.needToEatThreshold)
+            case State.MANAGEFOOD:
+             /*   if (lbBlackboard.hunger > lbBlackboard.needToEatThreshold)
                 {
                     ChangeState(State.EAT);
                 }
@@ -76,11 +80,17 @@ public class LadybugGoForFood_State : FiniteStateMachine
 
                 if(SensingUtils.DistanceToTarget(gameObject, arriveAvoid.target) < lbBlackboard.distanceToInteract)
                 {
+                    if (lbBlackboard.nest.ChildNeedFood())
+                    {
+                        lbBlackboard.nest.GiveFood();
+                    }
+                    else
+                    { 
+                        lbBlackboard.nest.savedFood++;
+                    }
 
-                }
-
-                break;
-            case State.EAT:
+                    Destroy(lbBlackboard.antTarget);
+                }*/
                 break;
         }
     }
@@ -94,10 +104,9 @@ public class LadybugGoForFood_State : FiniteStateMachine
             case State.PERSUE:
                 arriveAvoid.enabled = false;
                 break;
-            case State.BRINGBASE:
+            case State.MANAGEFOOD:
                 arriveAvoid.enabled = false;
-                break;
-            case State.EAT:
+                manageFood.Exit();
                 break;
         }
 
@@ -109,11 +118,10 @@ public class LadybugGoForFood_State : FiniteStateMachine
                 arriveAvoid.enabled = true;
                 arriveAvoid.target = lbBlackboard.antTarget;
                 break;
-            case State.BRINGBASE:
+            case State.MANAGEFOOD:
                 arriveAvoid.enabled = true;
+                manageFood.ReEnter();
                 arriveAvoid.target = lbBlackboard.nest.FoodTarget();
-                break;
-            case State.EAT:
                 break;
         }
 
---
+++ IA_01/Assets/Scripts/FSM/Ladybug/LadybugManageFood_State.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Steerings;
+using FSM;
+
+[RequireComponent(typeof(LadybugBlackboard))]
+public class LadybugManageFood_State : FiniteStateMachine
+{
+    public enum States
+    {
+        INITIAL, EAT, BRINGBASE
+    }
+
+    public States currentState = States.INITIAL;
+
+    private void Awake()
+    {
+        
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void ReEnter()
+    {
+        base.ReEnter();
+    }
+
+    private void Update()
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                break;
+            case States.EAT:
+                break;
+            case States.BRINGBASE:
+                break;
+        }
+    }
+
+    private void ChangeState(States newState)
+    {
+        switch (currentState)
+        {
+            case States.INITIAL:
+                break;
+            case States.EAT:
+                break;
+            case States.BRINGBASE:
+                break;
+        }
+
+        switch (newState)
+        {
+            case States.INITIAL:
+                break;
+            case States.EAT:
+                break;
+            case States.BRINGBASE:
+                break;
+        }
+
+        currentState = newState;
+    }
+}
