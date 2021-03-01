17fe8991bf761bdfb5e64f14bfffbe40e32b012c
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-03-01 18:41:52 +0100

    SpiderReDo

---
+++ IA_01/Assets/Scripts/FSM/Spider/SpiderBlackboard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBlackboard : MonoBehaviour
+{
+
+
+    public GameObject spiderHouse;
+
+    public float takeFoodRadius = 1f;
+
+
+    private void Awake()
+    {
+        if (spiderHouse == null)
+        {
+            spiderHouse = GameObject.FindObjectOfType<SpiderHouse>().gameObject;
+            if (spiderHouse == null)
+            {
+                Debug.LogError("No spider house found");
+            }
+        }
+    }
+}
