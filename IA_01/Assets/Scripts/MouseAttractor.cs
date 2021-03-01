65bf666cc9e2b468ca6edbe7f5fc023fa1ca741e
JoanStark <47462893+JoanStark@users.noreply.github.com>
2021-03-01 20:29:45 +0100

    Mouse Changes

--- IA_01/Assets/Scripts/MouseAttractor.cs
+++ IA_01/Assets/Scripts/MouseAttractor.cs
@@ -6,9 +6,18 @@ public class MouseAttractor : MonoBehaviour
 {
     Camera cam;
 
+    float deathZoneHeight;
+    float deathZoneWidth;
+
+    private Vector3 lastPos;
+
     private void Awake()
     {
+
         cam = Camera.main;
+
+        deathZoneHeight = Camera.main.orthographicSize;
+        deathZoneWidth = Camera.main.aspect * deathZoneHeight;
     }
 
     void Update()
@@ -16,5 +25,30 @@ public class MouseAttractor : MonoBehaviour
         Vector3 worldPosition = cam.ScreenToWorldPoint(Input.mousePosition);
 
         transform.position = (Vector2)worldPosition;
+
+        Bounds cameraBounds;
+
+        cameraBounds = new Bounds(cam.transform.position, new Vector3(2f * deathZoneWidth, 2f * deathZoneHeight, 100f));
+
+        if (!cameraBounds.Contains(transform.position))
+        {
+            Vector3 cp = cameraBounds.ClosestPoint(transform.position);
+
+            print(cp);
+            print(transform.position);
+
+            transform.position -= new Vector3(transform.position.x - cp.x, transform.position.y - cp.y);
+          
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        deathZoneHeight = Camera.main.orthographicSize;
+        deathZoneWidth = Camera.main.aspect * deathZoneHeight;
+
+        Gizmos.color = Color.cyan;
+
+        Gizmos.DrawWireCube(Camera.main.transform.position, new Vector3(2 * deathZoneWidth, 2 * deathZoneHeight, 5));
     }
 }
