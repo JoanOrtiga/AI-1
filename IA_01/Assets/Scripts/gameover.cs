69c850f567c6cf20969f1af45d6b6a13fd36d3f8
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-03-02 22:31:19 +0100

    Gameover

---
+++ IA_01/Assets/Scripts/gameover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameover : MonoBehaviour
+{
+    public GameObject gameoverCanvas;
+    private void Awake()
+    {
+        gameoverCanvas.SetActive(false);
+    }
+    private void Update()
+    {
+        if (GameObject.FindGameObjectsWithTag("ANT").Length <= 0)
+        {
+            gameoverCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
